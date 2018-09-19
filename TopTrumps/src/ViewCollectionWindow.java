6cf63884476a8a2aa3fb37f453dcfd849772c7fd
Kotwal <luqmaan.a.kotwal@accenture.com>
2018-09-19 13:22:24 +0100

    1322 update

--- TopTrumps/src/ViewCollectionWindow.java
+++ TopTrumps/src/ViewCollectionWindow.java
@@ -1,4 +1,48 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ViewCollectionWindow {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ViewCollectionWindow extends JFrame{
+	JPanel panel;
+	JButton home;
+	
+	public ViewCollectionWindow() {
+		this.setBounds(100, 100, 1200, 800);
+		InitialiseComponents();
+		this.setVisible(true);
+		
+	}
+
+	private void InitialiseComponents() {
+		// TODO Auto-generated method stub
+		panel = new JPanel();
+		panel.setBounds(0, 0, this.getWidth(), this.getHeight());
+		panel.setLayout(null);
+		this.getContentPane().add(panel);
+		
+		home = new JButton();
+		home.setBounds(1000, 100, 100, 100);
+		home.setText("Home");
+		home.addActionListener(
+				new ActionListener(){
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						// TODO Auto-generated method stub
+						MainPage mainpagewindow = new MainPage();
+						setVisible(false);
+						
+					}
+					
+				}
+				
+				);
+		panel.add(home);
+		
+	}
+	
 
 }
